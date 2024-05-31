551dc755f86da9de3632f377bb0f0e01b52d4a18
mik2003 <62058516+mik2003@users.noreply.github.com>
2024-05-31 17:24:33 +0200

    Fix YT player

--- docs/js/youtube_playlist.js
+++ docs/js/youtube_playlist.js
@@ -3,6 +3,7 @@ let currentTrackIndex = 0;
 let shuffle = false;
 let repeat = false;
 let player;
+let playerReady = false;
 
 document.addEventListener('DOMContentLoaded', () => {
     fetch('../data/animelist.json')
@@ -13,10 +14,8 @@ document.addEventListener('DOMContentLoaded', () => {
             return response.json();
         })
         .then(data => {
-            console.log('JSON data loaded successfully:', data); // Check if JSON is loaded
+            console.log('JSON data loaded successfully:', data);
             buildPlaylist(data);
-            onYouTubeIframeAPIReady();  // Ensure the player is ready
-            loadTrack(0);
         })
         .catch(error => console.error('Error loading JSON:', error));
 });
@@ -26,9 +25,15 @@ function onYouTubeIframeAPIReady() {
         height: '315',
         width: '560',
         events: {
+            'onReady': onPlayerReady,
             'onStateChange': onPlayerStateChange
         }
     });
+}
+
+function onPlayerReady(event) {
+    playerReady = true;
+    loadTrack(0);
 }
 
 function onPlayerStateChange(event) {
@@ -49,7 +54,7 @@ function buildPlaylist(data) {
             playlistElement.appendChild(li);
         });
     });
-    console.log('Playlist built:', playlist); // Check if playlist is built correctly
+    console.log('Playlist built:', playlist);
 }
 
 function extractVideoId(url) {
@@ -63,16 +68,16 @@ function extractVideoId(url) {
 }
 
 function loadTrack(index) {
-    if (index >= 0 && index < playlist.length) {
+    if (playerReady && index >= 0 && index < playlist.length) {
         currentTrackIndex = index;
         const track = playlist[currentTrackIndex];
         const currentTrackElement = document.getElementById('current-track');
         currentTrackElement.innerHTML = `Current Track: <a href="${track.yt_url}" target="_blank">${track.name} by ${track.artist}</a>`;
         const videoId = extractVideoId(track.yt_url);
-        player.loadVideoById(videoId);  // Load the video and start playing
-        console.log('Loaded track:', track); // Check if track is loaded correctly
+        player.loadVideoById(videoId);  // Load and start the video
+        console.log('Loaded track:', track);
     } else {
-        console.error('Invalid track index:', index);
+        console.error('Invalid track index or player not ready:', index);
     }
 }
 
