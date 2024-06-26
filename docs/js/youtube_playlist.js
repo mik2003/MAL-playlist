1033daee6ef0f7b990c72258eb477c3f950dedbf
mik2003 <62058516+mik2003@users.noreply.github.com>
2024-06-26 21:56:02 +0200

    CSS Styling

--- docs/js/youtube_playlist.js
+++ docs/js/youtube_playlist.js
@@ -8,6 +8,38 @@ var i, j, k, n = 0;
 var player;
 var loop = true;
 
+// https://stackoverflow.com/questions/45408920/plain-javascript-scrollintoview-inside-div
+function scrollParentToChild(parent, child) {
+
+    // Where is the parent on page
+    var parentRect = parent.getBoundingClientRect();
+    // What can you see?
+    var parentViewableArea = {
+        height: parent.clientHeight,
+        width: parent.clientWidth
+    };
+
+    // Where is the child
+    var childRect = child.getBoundingClientRect();
+    // Is the child viewable?
+    var isViewable = (childRect.top >= parentRect.top) && (childRect.bottom <= parentRect.top + parentViewableArea.height);
+
+    // if you can't see the child try to scroll parent
+    if (!isViewable) {
+        // Should we scroll using top or bottom? Find the smaller ABS adjustment
+        const scrollTop = childRect.top - parentRect.top;
+        const scrollBot = childRect.bottom - parentRect.bottom;
+        if (Math.abs(scrollTop) < Math.abs(scrollBot)) {
+            // we're near the top of the list
+            parent.scrollTop += scrollTop;
+        } else {
+            // we're near the bottom of the list
+            parent.scrollTop += scrollBot;
+        }
+    }
+
+}
+
 // Function to create an array with n integers
 function createArray(n) {
     let array = [];
@@ -198,8 +230,9 @@ function clearSongDivStyle() {
 
 function applySongDivStyle() {
     songDiv = document.getElementById(n);
-    songDiv.style.color = "#0000FF";
-    songDiv.scrollIntoView();
+    songDiv.style.color = "#4a8bad";
+    scrollParentToChild(document.getElementById("playlist"), songDiv);
+    // songDiv.scrollIntoView();
 }
 
 function goToNextSong() {
